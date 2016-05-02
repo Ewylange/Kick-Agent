2ccf9a7407e8376d35de7621a69d68800de647ee
Charlene ROSE <charlenerose1208@gmail.com>
2016-05-02 21:26:32 +0200

    Mise en place NavMesh

---
+++ Kick Agent/Assets/Scripts/AgentScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentScript : MonoBehaviour {
+
+
+	NavMeshAgent agent;
+	public GameObject GatherPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		agent.SetDestination(GatherPoint.transform.position);
+	
+	}
+}
