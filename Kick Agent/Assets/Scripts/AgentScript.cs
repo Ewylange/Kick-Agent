81615d2270183a00707b6173f4e94eeac3827d26
Charlene ROSE <charlenerose1208@gmail.com>
2016-05-04 19:42:37 +0200

    Update AgentScript ( CanKick Agent )

--- Kick Agent/Assets/Scripts/AgentScript.cs
+++ Kick Agent/Assets/Scripts/AgentScript.cs
@@ -6,6 +6,8 @@ public class AgentScript : MonoBehaviour {
 
 	NavMeshAgent agent;
 	public GameObject GatherPoint;
+	public float distanceToGatherPoint;
+	public bool canKickAgent;
 
 	// Use this for initialization
 	void Start () 
@@ -18,6 +20,18 @@ public class AgentScript : MonoBehaviour {
 	void Update () 
 	{
 		agent.SetDestination(GatherPoint.transform.position);
+		distanceToGatherPoint =  Vector3.Distance(GatherPoint.transform.position, transform.position);
+		if(distanceToGatherPoint < 1.5f) 
+		{	
+			Debug.Log("Agent on Gather Point ");
+			// Détruire et enlever des points ? TRouver un truc loufoque ou inhabituel a faire si l'ennemi atteint le point 
+
+		}
+		canKickAgent = false;
+		if (distanceToGatherPoint < 10) 
+		{
+			canKickAgent = true;
+		}
 	
 	}
 }
