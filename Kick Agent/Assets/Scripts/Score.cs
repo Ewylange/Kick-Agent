10e3fefcb6cf1ac155502ae750caa9efce9a7c4d
Gdhasylum <gdhasylum666@gmail.com>
2016-05-05 10:43:17 +0200

    Complétion du script Score et premier jet pour la sauvegarde du score

--- Kick Agent/Assets/Scripts/Score.cs
+++ Kick Agent/Assets/Scripts/Score.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class Score : MonoBehaviour {
 
+	public Text scoreText;
+	public int score = 0;
+	public int highScore = 0;
+
+	public bool autoSave = true ;
+
+	BinaryFormatter formatter = new BinaryFormatter();
+
+
 	// Use this for initialization
 	void Start () {
+
+		UpdateTexts ();
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void UpdateTexts () {
+
+		scoreText.text = score.ToString();
+	}
+
+	public void IncrementScore () {
+
+		score += 10 ;
+
+		if (score > highScore) {
+
+			highScore = score;
+		}
+
+		if (autoSave)
+			SaveHighscore ();
+
+		UpdateTexts ();
+	}
+
+	public void SaveHighscore () {
+
+
 	}
 }
