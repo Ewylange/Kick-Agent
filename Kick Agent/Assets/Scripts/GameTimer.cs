e0e5ae5a865bf391962efca1c6e73c64cdadd10c
Gdhasylum <gdhasylum666@gmail.com>
2016-05-05 21:49:06 +0200

    Mise en place d'un timer + premier layout du menu time's up !

---
+++ Kick Agent/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public Text timeText;
+
+	public float time = 512f;
+
+	public GameObject menuPause;
+
+	// Use this for initialization
+	void Start () {
+
+		UpdateTexts ();
+		menuPause.SetActive (false);
+	
+	}
+	void Update (){
+
+		DecrementTimer ();
+	}
+
+	void UpdateTexts () {
+	    
+		timeText.text = time.ToString();
+
+	}
+
+	public void DecrementTimer(){
+
+		time --;
+
+		if (time <= 0) {
+
+			Time.timeScale = 0f;
+			time = 0;
+			menuPause.SetActive(true);
+		}
+
+		UpdateTexts ();
+	}
+}
