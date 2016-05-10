0dd7372183b3cd645a2dcc9ee2a95579d854e462
Charlene ROSE <charlenerose1208@gmail.com>
2016-05-10 15:07:47 +0200

    Create AutoSave

---
+++ Kick Agent/Assets/Scripts/AutoSave.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class AutoSave : MonoBehaviour 
+{
+	public Score _scriptScore;
+
+	Toggle toggleComponent;
+	// Use this for initialization
+	void Start () 
+	{
+		toggleComponent = GetComponent<Toggle>();
+		toggleComponent.isOn = _scriptScore.autoSave;
+	}
+
+	public void OnToggle() 
+	{
+		_scriptScore.autoSave = toggleComponent.isOn;
+	}
+}
